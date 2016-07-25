c26f5317288193bc70713376887f5e60833b8e97
cohki0305 <cohki0305@hotmail.co.jp>
2016-07-25 22:34:39 +0900

    commit text

--- books-project/config/application.rb
+++ books-project/config/application.rb
@@ -22,5 +22,6 @@ module BooksProject
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #text
   end
 end
