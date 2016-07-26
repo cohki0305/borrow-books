fbb12bfc75df5350ac62ea53dc7fb89031029f26
cohki0305 <cohki0305@hotmail.co.jp>
2016-07-26 22:52:58 +0900

    update gitignore

--- books-project/db/migrate/20160726001542_change_isbn_to_books.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIsbnToBooks < ActiveRecord::Migration
-  def change
-    change_column :books, :isbn, :string
-  end
-end
--- books-project/db/migrate/20160726134935_add_editorial_review_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddEditorialReviewToBooks < ActiveRecord::Migration
-  def change
-    add_column :books, :editorial_review, :text
-  end
-end
