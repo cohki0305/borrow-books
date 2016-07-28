fa3a7a0217f667771ecbbc3b831ee2b591714036
cohki0305 <cohki0305@hotmail.co.jp>
2016-07-28 20:15:19 +0900

    you can see posted reviews

--- books-project/app/controllers/books_controller.rb
+++ books-project/app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @review = @book.reviews.build
+    @reviews = @book.reviews.all
   end
 
   def search
