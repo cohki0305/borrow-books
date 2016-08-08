ab4dd4cde9768a1ab78bd6c5b5a61c6a4290ea33
cohki0305 <cohki0305@hotmail.co.jp>
2016-08-08 16:28:09 +0900

    validates review, presence true

--- books-project/app/controllers/borrows_controller.rb
+++ books-project/app/controllers/borrows_controller.rb
@@ -7,10 +7,13 @@ class BorrowsController < ApplicationController
     book = Book.find_by(id: book_id)
     borrow = current_user.borrows.build(book_id: book.id, return_date: return_date, status: true)
 
-    if borrow.save
-      redirect_to show_books_path(book)
-    else
-      redirect_to root_path
+    if book.borrowable?
+      if borrow.save
+        redirect_to show_books_path(book)
+      else
+        redirect_to root_path
+      end
     end
   end
+  
 end
--- books-project/app/models/review.rb
+++ books-project/app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :book
+  validates :degree, presence: true
+  validates :Content, presence: true
+
 end
