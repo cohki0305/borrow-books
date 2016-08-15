5fca417863a06cf306f28daa54eab2342cc110d1
cohki0305 <cohki0305@hotmail.co.jp>
2016-08-15 20:04:11 +0900

    added borrowing status to book-show

--- books-project/app/helpers/books_helper.rb
+++ books-project/app/helpers/books_helper.rb
@@ -7,6 +7,17 @@ module BooksHelper
     end
 
     return average/reviews.length.to_f.round(3)
-
   end
+
+  def who_is_borrowing?(book)
+    user_id = book.borrows.find_by(status: true).user_id
+    user_email = User.find(user_id).email
+    return user_email
+  end
+
+  def id_of_man_borrowing(book)
+    user_id = book.borrows.find_by(status: true).user_id
+    return user_id
+  end
+
 end
