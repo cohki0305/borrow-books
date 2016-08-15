1f13360f92caa6e362af29f007c35f448c7a39fc
cohki0305 <cohki0305@hotmail.co.jp>
2016-08-15 19:52:33 +0900

    added review average to show of book

--- books-project/app/controllers/pages_controller.rb
+++ books-project/app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @ranking_reviews = Review.order("degree desc").limit(3)
   end
 end
--- books-project/app/helpers/books_helper.rb
+++ books-project/app/helpers/books_helper.rb
@@ -1,2 +1,12 @@
 module BooksHelper
+  def average_degree(reviews)
+    average = 0
+
+    reviews.each do |review|
+      average += review.degree
+    end
+
+    return average/reviews.length.to_f.round(3)
+
+  end
 end
