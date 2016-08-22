ada3161cc4b8cb806a37a97d775456f7a27ee2c7
cohki0305 <cohki0305@hotmail.co.jp>
2016-08-22 09:52:25 +0900

    added icon of user

--- books-project/app/controllers/pages_controller.rb
+++ books-project/app/controllers/pages_controller.rb
@@ -10,7 +10,8 @@ class PagesController < ApplicationController
 
     @all_books.each do |book|
       if book.reviews.count >= 1
-        @tmp_array << [book, (book.reviews.pluck(:degree).inject(0){|r,i| r+=i }/book.reviews.size).round(3)]
+        average_degree = (book.reviews.pluck(:degree).inject(0.0){|r,i| r+=i }/book.reviews.size).round(1)
+        @tmp_array << [book, average_degree]
       else
         @tmp_array << [book, 0]
       end
@@ -20,6 +21,5 @@ class PagesController < ApplicationController
 
     @top_four_books = @tmp_array[0..4]
   end
-
 
 end
